ef6110113ce6861a2f16fbc853de92ef6183730a
caio <caio_l_borba@estudante.sesisenai.org.br>
2026-05-08 15:28:22 -0300

    ex02

---
+++ scripts/ex02.js
@@ -0,0 +1,20 @@
+//console.log("Hello World");
+
+function contagemRegressiva(){
+
+        alert("se prepare para a contagem");
+    for (let i = 10; i >= 0; i--) {
+
+        alert(i);
+
+        console.log(i);
+
+        if (i < 1) {
+            alert("lançamento realizado");
+        }
+
+    }
+
+    
+
+}
